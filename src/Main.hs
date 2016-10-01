b561adf2633ace282581f3a4944c086a67432862
Jonathan Lange <jml@mumak.net>
2016-10-01 12:17:17 +0100

    Simple script depending on icu libraries

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,8 @@
 module Main (main) where
 
+import Data.Text.ICU.Replace (replaceAll)
+import Data.Text.IO (interact)
 import Protolude
 
 main :: IO ()
-main = do
-  putText "hello world"
+main = interact (replaceAll "jml" "jml of the shaven yak")
